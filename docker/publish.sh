b0f71a5368465d552975da782c82b24679bb5727
MaxTeselkin <tesmaxxxx@gmail.com>
2023-07-21 20:50:32 +0400

    experiment

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/owl-vit-gpu:1.0.0 . && \
-docker push supervisely/owl-vit-gpu:1.0.0
+docker build -t supervisely/owl-vit-gpu:1.0.1 . && \
+docker push supervisely/owl-vit-gpu:1.0.1
